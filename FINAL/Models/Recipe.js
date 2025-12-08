cae8cca8e714ed5901c2dad25463dff39e919383
giannascuteri903 <grs124@miami.edu>
2025-12-08 21:12:35 +0000

    change

--- FINAL/Models/Recipe.js
+++ FINAL/Models/Recipe.js
@@ -1,5 +1,5 @@
+const { sequelize } = require("./index");  
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db");
 
 const Recipe = sequelize.define("Recipe", {
     title: {
