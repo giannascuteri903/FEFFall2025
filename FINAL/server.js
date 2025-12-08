e3f8bc4c2e58c48c6e6e3d0005bc97ac1825a148
giannascuteri903 <grs124@miami.edu>
2025-12-08 21:14:00 +0000

    x

--- FINAL/server.js
+++ FINAL/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const sequelize = require('./Models');
+const { sequelize } = require('./Models');  // 
 const Recipe = require('./Models/Recipe');
 
 const app = express();
